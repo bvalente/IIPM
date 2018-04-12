cd1fe663e16ad9edd99ac934bb2cfecb6f2fafea
machadoatz <franciscocmachado@gmail.com>
2018-04-12 12:46:06 +0100

    jQuery at scale funcoes ainda têm lag

--- iRave/jQuery.js
+++ iRave/jQuery.js
@@ -5,42 +5,42 @@ jQuery(document).ready(function(){
 	const $lockScreen = $('.lockScreen');
 	const $mainScreen = $('.mainScreen');
 	var lock = true;
-	
-	
+
+
 	$lockButton.click(function(){
 		//hide everything else
 		if(!lock){
 			$clock.children().fadeOut();
 			$lockScreen.show();
-			$('#time').animate({top:'10vw', opacity:'1'}, "slow")
+			$('#time').animate({top:'0.3in', opacity:'1'}, "slow")
 			lock = true;
 		}
-		
+
 	});
-	
+
 	$('#touchToUnlock').click(function(){
 		lock = false;
-		$('#time').animate({top:'0vw',opacity:'0.1'}, "slow", function(){
+		$('#time').animate({top:'0in',opacity:'0.1'}, "slow", function(){
 			$lockScreen.fadeOut('fast');
 		} );
 		//$lockScreen.hide();
 		//$mainScreen.show();
 		showMenu();
 	});
-	
+
 	function showMenu(){
 		//$('#mainWidget').css('display', 'inline-block');
 		$('.mainScreen').fadeIn("slow");
 	}
-	
+
 	$('#mainWidget').click(function(){
 		$('.friends').fadeIn();
 		$('.mainScreen').fadeOut();
 	})
-	
+
 	$('.friends#back').click(function(){
 		$('.friends').fadeOut();
 		$('.mainScreen').fadeIn();
 	})
-	
+
 });
