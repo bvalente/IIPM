8db6ee8ca812d33e415c2748451e3fdf42d304c1
bvalente <bernardofvalente@gmail.com>
2018-04-13 12:22:51 +0100

    bug fix inches, mid-way function redo

--- iRave/jQuery.js
+++ iRave/jQuery.js
@@ -1,17 +1,30 @@
 jQuery(document).ready(function(){
-	const $clock = $('#clock')
+	const clock = $('#clock');
 	const $lockButton = $('#lockButton');
 	const $lockScreen = $('.lockScreen');
 	const $mainScreen = $('.mainScreen');
+	const MAIN_MENU = 0;
+	const FRIEND_LIST = 1;
+	const FRIEND_VIEW = 2;
 	var lock = true;
+	var menu = 0;
 
+	function lockScreen(){
+		clock.children().fadeOut(function(){
+			$('.mainScreen').fadeIn("slow");
+			$('#time').animate({top:'0.4', opacity:'1'}, "slow")
+		});
+	}
+	
+	function loadMainMenu(){
+		
+
+	}
 
 	$lockButton.click(function(){
 		//hide everything else
 		if(!lock){
-			$clock.children().fadeOut();
-			$lockScreen.show();
-			$('#time').animate({top:'0.5in', opacity:'1'}, "slow")
+			lockScreen();
 			lock = true;
 		}
 
@@ -27,23 +40,38 @@ jQuery(document).ready(function(){
 		showMenu();
 	});
 
-	function showMenu(){
-		//$('#mainWidget').css('display', 'inline-block');
-		$('.mainScreen').fadeIn("slow");
-	}
+
 
 	$('#mainWidget').click(function(){
 		$('.friends').fadeIn();
 		$('.mainScreen').fadeOut();
-	})
+		menu = friendsList;
+	});
+	
 
-	$('.friends#back').click(function(){
+	$('#back').click(function(){
 		$('.friends').fadeOut();
 		$('.mainScreen').fadeIn();
-	})
+		switch(menu){
+			case 2://friend view, load friendList
+				loadFriendsList();
+				menu = FRIEND_LIST;
+				break;
+			case 1://friendList, load main menu
+				loadMainMenu();
+				menu = FRIEND_VIEW;
+				break;
+			default: //error, load main menu
+				loadMenu();
+				menu = MAIN_MENU;
+				break;
+				   }
+	});
 
 	$('.friends#friend').click(function(){
-		$('.friends#friend').fadeOut();
-		$('.friendFunctions').fadeIn();
-	})
+		$('.friends#friend').fadeOut(function(){
+			$('.friendFunctions').fadeIn();
+		});
+		
+	});
 });
